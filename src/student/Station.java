04b49268503462c0e1d7c0fe8f915f26d23c9422
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-04-11 19:18:09 -0400

    finished station

--- src/student/Station.java
+++ src/student/Station.java
@@ -1,4 +1,61 @@
 package student;
 
-public class Station {
+import model.Space;
+
+/*
+Author @Alexander Hurley
+ */
+public class Station implements model.Station{
+
+    private String name;
+    private Integer col;
+    private Integer row;
+
+    /*
+    initializes values
+     */
+    public Station(String name, Integer col, Integer row){
+        this.name=name;
+        this.col=col;
+        this.row=row;
+    }
+
+    /*
+    returns the station name
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /*
+    returns the row of the station
+     */
+    @Override
+    public int getRow() {
+        return row;
+    }
+
+    /*
+    returns the column of the station
+     */
+    @Override
+    public int getCol() {
+        return col;
+    }
+
+    /*
+    returns whether or not the station occupies the same place in the board as the passed in space type
+     */
+    @Override
+    public boolean collocated(Space other) {
+
+        int row = other.getRow();
+        int col = other.getCol();
+
+        if (row == this.row && col == this.col){
+            return true;
+        }
+        return false;
+    }
 }

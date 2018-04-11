3eab058b7ba9ec7e4e6bc736dc8f12cc2f3cdc89
Brett Farruggia <brf3493@rit.edu>
2018-04-11 19:17:25 -0400

    Created Space Class.

--- src/student/Space.java
+++ src/student/Space.java
@@ -1,15 +1,42 @@
 package student;
 
-public class Space implements model.Space {
-    @Override
 
-    public int getRow() {
-        return 0;
+/**
+ * A space on the gameboard.
+ * Author: Brett Farruggia (brf3493)
+ */
+public class Space implements model.Space {
+
+
+    private Integer row;
+
+    private Integer col;
+
+    /**
+     * Creates a space.
+     */
+     public Space(){
+
+         this.row = row;
+         this.col = col;
+
+
     }
 
     @Override
+    /**
+     * Returns the row position of the space
+     */
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    /**
+     * Returns the column position of the space.
+     */
     public int getCol() {
-        return 0;
+        return col;
     }
 
     @Override

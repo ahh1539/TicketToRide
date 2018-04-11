417e687ef852dab77bdbd9ad2ac606708d7964bb
Brett Farruggia <brf3493@rit.edu>
2018-04-11 18:51:47 -0400

    stubbed class space

--- src/student/Space.java
+++ src/student/Space.java
@@ -1,4 +1,18 @@
 package student;
 
-public class Space {
+public class Space implements model.Space {
+    @Override
+    public int getRow() {
+        return 0;
+    }
+
+    @Override
+    public int getCol() {
+        return 0;
+    }
+
+    @Override
+    public boolean collocated(model.Space other) {
+        return false;
+    }
 }

c65174cc95a33b1646ee0c016f5d663271c4f430
Brett Farruggia <brf3493@rit.edu>
2018-04-13 16:34:15 -0400

    Bug Fix

--- src/student/StuRailRoadMapObserver.java
+++ src/student/StuRailRoadMapObserver.java
@@ -1,4 +1,21 @@
 package student;
 
-public class StuRailRoadMapObserver {
+import model.Player;
+import model.RailroadBarons;
+
+public class StuRailRoadMapObserver implements model.RailroadBaronsObserver {
+    @Override
+    public void turnStarted(RailroadBarons game, Player player) {
+
+    }
+
+    @Override
+    public void turnEnded(RailroadBarons game, Player player) {
+
+    }
+
+    @Override
+    public void gameOver(RailroadBarons game, Player winner) {
+
+    }
 }

968a816728cfa3944a1ff30c03830a31d70e6e3a
Brett Farruggia <brf3493@rit.edu>
2018-04-13 16:25:56 -0400

    Finished Deck.

--- src/student/StuDeck.java
+++ src/student/StuDeck.java
@@ -8,9 +8,27 @@ import java.util.Collections;
 public class StuDeck implements model.Deck {
 
     public static ArrayList<Card> gameDeck;
+
+    public StuDeck(){
+
+        for(int i = 1; i <=16; i++) {   //number of cards in a deck?
+            gameDeck.add(Card.BLACK);
+            gameDeck.add(Card.BLUE);
+            gameDeck.add(Card.ORANGE);
+            gameDeck.add(Card.GREEN);
+            gameDeck.add(Card.PINK);
+            gameDeck.add(Card.WHITE);
+            gameDeck.add(Card.RED);
+            gameDeck.add(Card.YELLOW);
+            gameDeck.add(Card.WILD);
+        }
+        Collections.shuffle(gameDeck);
+
+    }
     @Override
     public void reset() {
         gameDeck.clear();
+
         for(int i = 1; i <=16; i++) {   //number of cards in a deck?
            gameDeck.add(Card.BLACK);
            gameDeck.add(Card.BLUE);

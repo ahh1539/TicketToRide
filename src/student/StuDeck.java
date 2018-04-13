d4a0af3c7fb0660a6525cdbe2c21f5ff9317f1a5
Brett Farruggia <brf3493@rit.edu>
2018-04-13 15:09:29 -0400

    Implementing Deck

--- src/student/StuDeck.java
+++ src/student/StuDeck.java
@@ -2,19 +2,44 @@ package student;
 
 import model.Card;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class StuDeck implements model.Deck {
+
+    public static ArrayList<Card> gameDeck;
     @Override
     public void reset() {
+        gameDeck.clear();
+        for(int i = 1; i <=16; i++) {   //number of cards in a deck?
+           gameDeck.add(Card.BLACK);
+           gameDeck.add(Card.BLUE);
+           gameDeck.add(Card.ORANGE);
+           gameDeck.add(Card.GREEN);
+           gameDeck.add(Card.PINK);
+           gameDeck.add(Card.WHITE);
+           gameDeck.add(Card.RED);
+           gameDeck.add(Card.YELLOW);
+           gameDeck.add(Card.WILD);
+        }
+        Collections.shuffle(gameDeck);
+
 
     }
 
     @Override
     public Card drawACard() {
-        return null;
+        if (numberOfCardsRemaining() == 0){
+            return Card.NONE;
+
+        }else{
+            return gameDeck.remove(0);
+        }
     }
 
     @Override
     public int numberOfCardsRemaining() {
-        return 0;
+
+        return gameDeck.size();
     }
 }

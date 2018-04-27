4f53aaccef7444e921498f2661d17ae9966aa43d
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-04-27 19:39:09 -0400

    part one fixing 6

--- src/student/StuRailroadBarons.java
+++ src/student/StuRailroadBarons.java
@@ -21,7 +21,10 @@ public class StuRailroadBarons implements model.RailroadBarons  {
     private int playerRot;
     private ArrayList<RailroadBaronsObserver> observers = new ArrayList<>();
 
-
+    /*
+    constructor of game that adds players to board and sets the player
+    rotation to the beginning of the cycle
+     */
     public StuRailroadBarons() {
         players.add(new StuPlayer(Baron.RED));
         players.add(new StuPlayer(Baron.GREEN));
@@ -31,16 +34,27 @@ public class StuRailroadBarons implements model.RailroadBarons  {
         playerRot = 0;
     }
 
+    /*
+    adds new observer observers list to be notified when the state of the game changes
+     */
     @Override
     public void addRailroadBaronsObserver(RailroadBaronsObserver observer) {
         observers.add(observer);
     }
 
+    /*
+    removes an observer from the list of observers so that they are not notified
+    when the game state changes
+     */
     @Override
     public void removeRailroadBaronsObserver(RailroadBaronsObserver observer) {
         observers.remove(observer);
     }
 
+    /*
+    starts a new Railroad barons game and initializes all values to their start position and creates game deck
+    using a given map
+     */
     @Override
     public void startAGameWith(RailroadMap map) {
         this.map = map;
@@ -57,6 +71,10 @@ public class StuRailroadBarons implements model.RailroadBarons  {
         }
     }
 
+    /*
+    starts a new Railroad barons game and initializes all values to their start position using a given
+    map and deck
+     */
     @Override
     public void startAGameWith(RailroadMap map, Deck deck) {
         deck = new StuDeck();
@@ -73,21 +91,33 @@ public class StuRailroadBarons implements model.RailroadBarons  {
         }
     }
 
+    /*
+    returns the gameboard map
+     */
     @Override
     public RailroadMap getRailroadMap() {
         return map;
     }
 
+    /*
+    returns the number of unassigned cards left in the deck
+     */
     @Override
     public int numberOfCardsRemaining() {
         return deck.numberOfCardsRemaining();
     }
 
+    /*
+    returns a boolean stating whether or not a play is eligible to claim a route
+     */
     @Override
     public boolean canCurrentPlayerClaimRoute(int row, int col) {
         return (currentPlayer.canClaimRoute(map.getRoute(row,col)));
     }
 
+    /*
+    claims route fot player and updates the gameboard of that change
+     */
     @Override
     public void claimRoute(int row, int col) throws RailroadBaronsException {
         if (canCurrentPlayerClaimRoute(row,col)) {
@@ -96,10 +126,13 @@ public class StuRailroadBarons implements model.RailroadBarons  {
         }
     }
 
+    /*
+    ends a players turn chainging players rotation value to next player
+     */
     @Override
     public void endTurn() {
-        for (RailroadBaronsObserver r:observers) {
-            r.turnEnded(this,currentPlayer);
+        for (RailroadBaronsObserver obs:observers) {
+            obs.turnEnded(this,currentPlayer);
         }
         if (!gameIsOver()) {
             playerRot += 1;
@@ -108,22 +141,31 @@ public class StuRailroadBarons implements model.RailroadBarons  {
             }
             currentPlayer = players.get(playerRot);
             currentPlayer.startTurn(new StuPair(deck));
-            for (RailroadBaronsObserver r : observers) {
-                r.turnStarted(this, currentPlayer);
+            for (RailroadBaronsObserver obz : observers) {
+                obz.turnStarted(this, currentPlayer);
             }
         }
     }
 
+    /*
+      returns the player whose turn it currently is
+     */
     @Override
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
 
+    /*
+    returns all players currently in the game
+     */
     @Override
     public Collection<Player> getPlayers() {
         return players;
     }
 
+    /*
+    checks game status to see if game has met end requirenments and declairs a winner
+     */
     @Override
     public boolean gameIsOver() {
         boolean gameOver1 = false;

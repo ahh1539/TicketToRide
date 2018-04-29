0ce76c7b120f9f0f44a5bf5abc0f145361b8760c
Brett Farruggia <brf3493@rit.edu>
2018-04-29 18:32:02 -0400

    Working on AI implementation and Lonely version of the game

--- src/student/LonelyRailroadBarons.java
+++ src/student/LonelyRailroadBarons.java
@@ -2,67 +2,199 @@ package student;
 
 import model.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class LonelyRailroadBarons implements model.RailroadBarons{
+    private ArrayList<Player> players = new ArrayList<>();
+    private Player currentPlayer;
+    private RailroadMap map;
 
+    private Deck deck;
+    private int playerRot;
+    private ArrayList<RailroadBaronsObserver> observers = new ArrayList<>();
+
+    /*
+    constructor of game that adds players to board and sets the player
+    rotation to the beginning of the cycle
+     */
+    public LonelyRailroadBarons() {
+        players.add(new StuAIPlayer(Baron.RED));
+        players.add(new StuAIPlayer(Baron.GREEN));
+        players.add(new StuAIPlayer(Baron.YELLOW));
+        players.add(new StuPlayer(Baron.BLUE));
+        deck = new StuDeck();
+        playerRot = 0;
+    }
+
+    /*
+    adds new observer observers list to be notified when the state of the game changes
+     */
     @Override
     public void addRailroadBaronsObserver(RailroadBaronsObserver observer) {
-
+        observers.add(observer);
     }
 
+    /*
+    removes an observer from the list of observers so that they are not notified
+    when the game state changes
+     */
     @Override
     public void removeRailroadBaronsObserver(RailroadBaronsObserver observer) {
-
+        observers.remove(observer);
     }
 
+    /*
+    starts a new Railroad barons game and initializes all values to their start position and creates game deck
+    using a given map
+     */
     @Override
     public void startAGameWith(RailroadMap map) {
-
+        this.map = map;
+        deck = new StuDeck();
+        playerRot = 0;
+        for (Player p:players) {
+            p.reset(deck.drawACard(),deck.drawACard(),
+                    deck.drawACard(),deck.drawACard());
+        }
+        currentPlayer = players.get(playerRot);
+        currentPlayer.startTurn(new StuPair(deck));
+        for (RailroadBaronsObserver r:observers) {
+            r.turnStarted(this,currentPlayer);
+        }
     }
 
+    /*
+    starts a new Railroad barons game and initializes all values to their start position using a given
+    map and deck
+     */
     @Override
     public void startAGameWith(RailroadMap map, Deck deck) {
-
+        deck = new StuDeck();
+        playerRot = 0;
+        this.map = map;
+        for (Player p:players) {
+            p.reset(deck.drawACard(),deck.drawACard(),
+                    deck.drawACard(),deck.drawACard());
+        }
+        currentPlayer = players.get(playerRot);
+        currentPlayer.startTurn(new StuPair(deck));
+        for (RailroadBaronsObserver r:observers) {
+            r.turnStarted(this,currentPlayer);
+        }
     }
 
+    /*
+    returns the gameboard map
+     */
     @Override
     public RailroadMap getRailroadMap() {
-        return null;
+        return map;
     }
 
+    /*
+    returns the number of unassigned cards left in the deck
+     */
     @Override
     public int numberOfCardsRemaining() {
-        return 0;
+        return deck.numberOfCardsRemaining();
     }
 
+    /*
+    returns a boolean stating whether or not a play is eligible to claim a route
+     */
     @Override
     public boolean canCurrentPlayerClaimRoute(int row, int col) {
-        return false;
+        return (currentPlayer.canClaimRoute(map.getRoute(row,col)));
     }
 
+    /*
+    claims route fot player and updates the gameboard of that change
+     */
     @Override
     public void claimRoute(int row, int col) throws RailroadBaronsException {
-
+        if (canCurrentPlayerClaimRoute(row,col)) {
+            currentPlayer.claimRoute(map.getRoute(row,col));
+            map.routeClaimed(map.getRoute(row,col));
+        }
     }
 
+    /*
+    ends a players turn chainging players rotation value to next player
+     */
     @Override
     public void endTurn() {
-
+        for (RailroadBaronsObserver obs:observers) {
+            obs.turnEnded(this,currentPlayer);
+        }
+        if (!gameIsOver()) {
+            playerRot += 1;
+            if (playerRot == 4) {
+                playerRot = 0;
+            }
+            currentPlayer = players.get(playerRot);
+            currentPlayer.startTurn(new StuPair(deck));
+            for (RailroadBaronsObserver obz : observers) {
+                obz.turnStarted(this, currentPlayer);
+            }
+        }
     }
 
+    /*
+      returns the player whose turn it currently is
+     */
     @Override
     public Player getCurrentPlayer() {
-        return null;
+        return currentPlayer;
     }
 
+    /*
+    returns all players currently in the game
+     */
     @Override
     public Collection<Player> getPlayers() {
-        return null;
+        return players;
     }
 
+    /*
+    checks game status to see if game has met end requirenments and declairs a winner
+     */
     @Override
     public boolean gameIsOver() {
+        boolean gameOver1 = false;
+        boolean gameOver2 = false;
+        if (map.getLengthOfShortestUnclaimedRoute() == 999) {
+            gameOver1 = true;
+            gameOver2 = true;
+        }
+        if (deck.numberOfCardsRemaining() == 0) {
+            gameOver1 = true;
+        }
+        int x = 0;
+        for (Player p : players) {
+            if (!p.canContinuePlaying(map.getLengthOfShortestUnclaimedRoute())) {
+                x += 1;
+            }
+        }
+        if (x == 4&&deck.numberOfCardsRemaining()==0) {
+            gameOver1 = true;
+            gameOver2 = true;
+        }
+        if (gameOver1&&gameOver2) {
+            Player winner = null;
+            int highScore = 0;
+            for (Player p : players) {
+                if (p.getScore() >= highScore) {
+                    highScore = p.getScore();
+                    winner = p;
+                }
+            }
+            for (RailroadBaronsObserver r : observers) {
+                r.gameOver(this, winner);
+            }
+            return true;
+        }
         return false;
     }
 }
+
---
+++ src/student/StuAIPlayer.java
@@ -0,0 +1,311 @@
+package student;
+
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.TreeMap;
+
+public class StuAIPlayer implements model.Player {
+    private Card lastCard;
+    private Card secondLastCard;
+
+    private Baron baron;
+    private ArrayList<Route> claimedRoutes;
+    private TreeMap<Card, Integer> playerHand;
+    private boolean claimedThisTurn;
+
+    private int trains;
+    private int scoreTotal;
+    private Pair lastPair;
+
+    private boolean northSouthMultiplier;
+    private boolean eastWestMultiplier;
+
+
+
+    private ArrayList<PlayerObserver> observers = new ArrayList<>();
+
+    /**
+     * Constructor for BoardPlayer.
+     * @param baron
+     */
+    public StuAIPlayer(Baron baron) {
+        claimedThisTurn = false;
+        this.baron = baron;
+        claimedRoutes = new ArrayList<>();
+        playerHand = createHand();
+        trains = 45;
+        scoreTotal = 0;
+        lastCard = Card.NONE;
+        secondLastCard = Card.NONE;
+
+        northSouthMultiplier = false;
+        eastWestMultiplier = false;
+
+
+    }
+
+    /**
+     * @return - Cards in the player's playerHand.
+     */
+    public TreeMap<Card, Integer> createHand() {
+        TreeMap<Card,Integer> temp = new TreeMap<>();
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(Card.ORANGE);
+        cards.add(Card.RED);
+        cards.add(Card.BLACK);
+        cards.add(Card.WHITE);
+        cards.add(Card.BLUE);
+        cards.add(Card.YELLOW);
+        cards.add(Card.GREEN);
+        cards.add(Card.PINK);
+        cards.add(Card.WILD);
+        for (Card card: cards) {
+            temp.put(card,0);
+        }
+        return temp;
+    }
+
+    /**
+     * Resets the player's playerHand.
+     * @param dealt
+     */
+    @Override
+    public void reset(Card... dealt) {
+        trains = 45;
+        scoreTotal = 0;
+        claimedRoutes.clear();
+        lastCard = Card.NONE;
+        secondLastCard = Card.NONE;
+        for (int cards:playerHand.values()) {
+            cards = 0;
+        }
+        for (int x=0;x<4;x++) {
+            playerHand.put(dealt[x], playerHand.get(dealt[x]) + 1);
+        }
+        for (PlayerObserver p:observers) {
+            p.playerChanged(this);
+        }
+    }
+
+    /**
+     * Adds a player observer.
+     * @param observer The new {@link PlayerObserver}.
+     */
+    @Override
+    public void addPlayerObserver(PlayerObserver observer) {
+        observers.add(observer);
+    }
+
+    /**
+     * Removes a player observer.
+     * @param observer
+     */
+    @Override
+    public void removePlayerObserver(PlayerObserver observer) {
+        observers.add(observer);
+    }
+
+    /**
+     * @return - a player.
+     */
+    @Override
+    public Baron getBaron() {
+        return baron;
+    }
+
+    /**
+     * Begins the player's turn.
+     * @param dealt
+     */
+    @Override
+    public void startTurn(Pair dealt) {
+        lastPair = dealt;
+        for (Card c:playerHand.keySet()) {
+            if (c==lastPair.getFirstCard()) {
+                playerHand.put(c,playerHand.get(c)+1);
+            }
+            if (c==lastPair.getSecondCard()) {
+                playerHand.put(c,playerHand.get(c)+1);
+            }
+        }
+        claimedThisTurn = false;
+        for (PlayerObserver p:observers) {
+            p.playerChanged(this);
+        }
+    }
+
+    /**
+     * @return - Last pair of cards.
+     */
+    @Override
+    public Pair getLastTwoCards() {
+        return lastPair;
+    }
+
+    /**
+     * @param card The {@link Card} of interest.
+     * @return - Cards in the player's playerHand.
+     */
+    @Override
+    public int countCardsInHand(Card card) {
+        return playerHand.get(card);
+    }
+
+    /**
+     * @return - the number of train pieces on
+     * the board.
+     */
+    @Override
+    public int getNumberOfPieces() {
+        return trains;
+    }
+
+    /**
+     * Check
+     * @param route
+     * @return - boolean.
+     */
+    @Override
+    public boolean canClaimRoute(Route route) {
+        if (route.getBaron()==Baron.UNCLAIMED&&
+                getNumberOfPieces()>=route.getLength()&&
+                claimedThisTurn==false&&checkCardAmounts(route.getLength())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * A player claims a route.
+     * @param route The {@link Route} to claim.
+     *
+     * @throws RailroadBaronsException
+     */
+    @Override
+    public void claimRoute(Route route) throws RailroadBaronsException {
+        if (canClaimRoute(route)) {
+
+            route.claim(baron);
+            playCards(route);
+            claimedRoutes.add(route);
+            scoreTotal += route.getPointValue();
+            trains -= route.getLength();
+            claimedThisTurn = true;
+
+        }
+        for (PlayerObserver p:observers) {
+            p.playerChanged(this);
+        }
+    }
+
+    /**
+     * @return - routes claimed by a player.
+     */
+    @Override
+    public Collection<Route> getClaimedRoutes() {
+        return claimedRoutes;
+    }
+
+    /**
+     * @return - the player's scoreTotal.
+     */
+    @Override
+    public int getScore() {
+        return scoreTotal;
+    }
+
+
+
+    /**
+     * Checks to see if the game is over using routes.
+     * @param shortestUnclaimedRoute
+     *
+     * @return - boolean
+     */
+    @Override
+    public boolean canContinuePlaying(int shortestUnclaimedRoute) {
+        if (getNumberOfPieces()>=shortestUnclaimedRoute&&checkCardAmounts(shortestUnclaimedRoute)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks to see if game is over using card
+     * amounts.
+     * @param routeLength
+     * @return - boolean.
+     */
+    public boolean checkCardAmounts(int routeLength) {
+        int mostCards = 0;
+        int wildCards = 0;
+
+        for (Card card: playerHand.keySet()) {
+            if (card==Card.WILD) {
+                wildCards = playerHand.get(card);
+            }
+            else {
+                if (playerHand.get(card)>mostCards) {
+                    mostCards = playerHand.get(card);
+                }
+            }
+        }
+        if (mostCards >= routeLength) {
+            return true;
+        }
+        if (mostCards == routeLength-1 && wildCards > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Uses the player's cards to claim routes.
+     * @param route
+     */
+    public void playCards(Route route) {
+        boolean played = false;
+        for (Card card : playerHand.keySet()) {
+            if (card != Card.WILD) {
+                if (playerHand.get(card) >= route.getLength()) {
+                    playerHand.put(card, playerHand.get(card)-route.getLength());
+                    played = true;
+                    break;
+                }
+            }
+        }
+        if (!played) {
+            for (Card card : playerHand.keySet()) {
+                if (card != Card.WILD) {
+                    if (playerHand.get(card) == route.getLength() - 1 && playerHand.get(Card.WILD) >= 1) {
+                        playerHand.put(card, playerHand.get(card) - (route.getLength() - 1));
+                        playerHand.put(Card.WILD, playerHand.get(Card.WILD) - 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return - Name of Baron.
+     */
+    public String toString() {
+        if (baron==Baron.RED) {return "RED Baron";}
+        if (baron==Baron.YELLOW) {return "YELLOW Baron";}
+        if (baron==Baron.GREEN) {return "GREEN Baron";}
+        if (baron==Baron.BLUE) {return "BLUE Baron";}
+        return ("Failed");
+    }
+
+
+    public Boolean[] getMultiplier(){
+        Boolean multi[] = new Boolean[2];
+        multi[0] = northSouthMultiplier;
+        multi[1] = eastWestMultiplier;
+        return multi;
+    }
+}
+
